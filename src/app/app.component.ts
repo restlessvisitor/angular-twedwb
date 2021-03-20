9e56b7a530e6faf162bcb5404aaa035ff277f481
restlessvisitor <restless.visitor@gmail.com>
2021-03-20 19:54:04 +0100

    2 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from "./auth.service";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   constructor(private auth: AuthService) {}
