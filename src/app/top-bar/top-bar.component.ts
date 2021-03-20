56f16737230c45f9be7491406cee5418c212bf5e
restlessvisitor <restless.visitor@gmail.com>
2021-03-20 17:00:12 +0100

    2 files modified

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from "@angular/core";
+import { CartService } from "../cart.service";
 
 @Component({
   selector: "app-top-bar",
   templateUrl: "./top-bar.component.html",
   styleUrls: ["./top-bar.component.css"]
 })
-export class TopBarComponent {}
+export class TopBarComponent {
+  constructor(private cart: CartService) {}
+
+  getCartCount() {
+    return this.cart.items.length;
+  }
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
