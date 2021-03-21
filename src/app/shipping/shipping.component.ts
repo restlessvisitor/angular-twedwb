4eadc931b36040826ce7103a6a0a4831171af426
restlessvisitor <restless.visitor@gmail.com>
2021-03-21 20:08:15 +0100

    shipping: expandable table added

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
 import { Shipping } from '../shipping';
@@ -5,12 +6,20 @@ import { Shipping } from '../shipping';
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
-  styleUrls: ['./shipping.component.css']
+  styleUrls: ['./shipping.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class ShippingComponent implements OnInit {
   shippingCosts: Shipping[];
   displayedColumns: string[] = ['type', 'price'];
-  
+  expandedElement: Shipping | null;
+
   constructor(private cartService: CartService) { }
 
   ngOnInit() {
