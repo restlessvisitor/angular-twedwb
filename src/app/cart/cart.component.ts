f7f13a12b742be00fdbb7c7de595057bb6bc297a
restlessvisitor <restless.visitor@gmail.com>
2021-03-21 20:53:51 +0100

    checkout table is expandable now

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,19 +2,28 @@ import { Component, OnInit } from "@angular/core";
 import { CartService } from "../cart.service";
 import { FormBuilder } from "@angular/forms";
 import { Product } from "../product";
+import { animate, state, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: "app-cart",
   templateUrl: "./cart.component.html",
-  styleUrls: ["./cart.component.css"]
+  styleUrls: ["./cart.component.css"],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class CartComponent implements OnInit {
   items: Product[];
-  displayedColumns: string[] = ["actions", "name", "price"];
+  displayedColumns: string[] = ["name", "price"];
   checkoutForm = this.formBuilder.group({
     name: "",
     address: ""
   });
+  expandedElement: Product | null;
 
   constructor(
     private cartService: CartService,
